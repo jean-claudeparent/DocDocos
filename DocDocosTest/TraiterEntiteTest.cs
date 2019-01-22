63163cad9123507128a3f645231711bd5e47772e
Jean-Claude Parent <noursicain@gmail.com>
2019-01-22 14:33:02 -0500

    Squelette de la classe de test des paramètres

---
+++ DocDocosTest/TraiterEntiteTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DocDocos;
+using System;
+
+
+namespace DocDocosTest
+{
+    /// <summary>
+    /// Tester ;a transformation du
+    /// fragment xml en entite
+    /// de document
+    /// </summary>
+    [TestClass]
+    public class TraiterEntiteTest
+    {
+        [TestMethod]
+        public void ParametresTest()
+        {
+            Assert.Fail("pas encore implémenté");
+        }
+
+
+    }
+}
