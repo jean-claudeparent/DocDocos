d81df22046c085c357096599828ba59631ecfd02
Jean-Claude Parent <noursicain@gmail.com>
2019-02-09 11:23:59 -0500

    Implémentation de DocDocosCLI

--- DocDocosCLI/Program.cs
+++ DocDocosCLI/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.IO;
+
 
 namespace DocDocosCLI
 {
     class Program
     {
+        static string monRepertoireSortie;
+        static string monFichierXML;
+        static string monGabarit;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (ArgumentInvalide(args))
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine(
+                    "     DocFocos -f ficgierdix.xml " +
+                    "-r repertoiresortie [-g gabarit.html]");
+                Environment.Exit(99);  
+
+            }
+
+            if(!File.Exists(monFichierXML))
+            {
+                Console.WriteLine("Il y a un problème avec le fichier xml " +
+                    monFichierXML);
+                Environment.Exit(99);  
+            }
+
+            if ((!string.IsNullOrEmpty(monGabarit )) && 
+                (!File.Exists(monGabarit)))
+            {
+                Console.WriteLine("Il y a un problème avec le fichier de gabarit " +
+                    monGabarit );
+                Environment.Exit(99);
+            }
+        }
+
+        private static bool ArgumentInvalide(
+            string[] args)
+        {
+            if (args.Length < 4 )
+                return false;
+            for (int i=0; i < (args.Length - 1); i++)
+            {
+                if (args[i].ToUpper() == "-F")
+                    monFichierXML = args[i+1];
+                if (args[i].ToUpper() == "-G")
+                    monGabarit  = args[i + 1];
+                if (args[i].ToUpper() == "-R")
+                    monRepertoireSortie  = args[i + 1];
+            }
+            if (string.IsNullOrEmpty(monFichierXML))
+                return false ;
+            if (string.IsNullOrEmpty(monRepertoireSortie ))
+                return false;
+
+            return true; 
         }
     }
 }
